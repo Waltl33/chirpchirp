31a64006711ece440d0d70d6756441cf4f3bd3a1
Adam Romero <aromero0904@gmail.com>
2023-02-15 10:37:26 -0500

    Settings Component

--- frontend/chirpchirp/src/components/Profile.js
+++ frontend/chirpchirp/src/components/Profile.js
@@ -2,6 +2,8 @@ import {React, useContext } from 'react'
 import { useParams } from 'react-router-dom'
 import { GlobalContext } from '../context/user.js'
 import Header from "./Header.js"
+import Nav from './Nav.js'
+import Settings from './Settings.js'
 
 export default function Profile() {
   // import params from url
@@ -14,13 +16,18 @@ export default function Profile() {
   globalState.page = 'profile';
   
   return (
+    
     globalState.isLoggedIn? 
     <>
       <Header />
+      <Nav/>
+      <img src={globalState.pfp}/> <img src={"bannerImage"}/>
       <h6>Logged In as {globalState.username}</h6>
     </> : 
     <>
      <Header />
+     <Nav/>
+     <img src={globalState.pfp}/> <img src={"bannerImage"}/>
      <h6>Not logged in</h6>
     </>
   )
